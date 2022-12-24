a501abf1897fe8ccb257d08e9b876e77ddc4fe7d
Minghong Xu <86758413+MinghongAlexXu@users.noreply.github.com>
2022-12-24 15:53:31 +0000

    PPO is classified as an actor-critic method

--- src/deeprl/actor_critic_methods/__init__.py
+++ src/deeprl/actor_critic_methods/__init__.py
@@ -1,8 +1,10 @@
 from .ddpg import DDPG
+from .ppo import PPO
 from .sac import SAC
 from .td3 import TD3
 
 __all__ = (
+    PPO.__name__,
     DDPG.__name__,
     TD3.__name__,
     SAC.__name__,
---
+++ src/deeprl/actor_critic_methods/ppo.py
@@ -0,0 +1,3 @@
+class PPO:
+    def __init__(self) -> None:
+        raise NotImplementedError("Proximal Policy Optimization")
